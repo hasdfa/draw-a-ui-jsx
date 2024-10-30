6226de19c6b423871a91c0e4073836b2f600aa7f
Vadym Raksha <hello@raksha.bio>
2024-10-30 12:35:51 +0000

    Update route.ts

--- app/api/toHtml/route.ts
+++ app/api/toHtml/route.ts
@@ -34,7 +34,7 @@ const userPrompt = `
 You are tasked with converting an image into a single JavaScript file using @mui/material components. Your goal is to create a functional and visually accurate representation of the given UI using Material-UI components.
 `
 
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60;
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
